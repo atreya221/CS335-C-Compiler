afdeb245164569ceb1c3ec8db2636aa6c77a7059
divigarg <divigarg@iitk.ac.in>
2022-03-28 23:08:52 +0530

    Changes made
    Co-authored-by: Atreya Goswami <atreya@iitk.ac.in>
    Co-authored-by: Anjali Rana <AnjaliRana20@users.noreply.github.com>
    Co-authored-by: Lipi Jain <lipijain2@users.noreply.github.com>

--- inc/expression.h
+++ inc/expression.h
@@ -2,18 +2,18 @@
 #define __EXPRESSION_H__
 
 #include "ast.h"
+#include "symtab.h"
+#include <string>
 #include <deque>
 #include <map>
-#include <string>
-#include "symtab.h"
 
 using namespace std;
 
-// All class declarations
+
 class StructDefinition;
-class AssignmentExpression;
 class ParameterTypeList;
 class CastExpression;
+class AssignmentExpression;
 class Constant;
 class StringLiteral;
 class TopLevelExpression;
@@ -21,48 +21,32 @@ class TopLevelExpression;
 class Expression : public Non_Terminal {
   public:
     Type type;
-    /* Change this late */
     int num_opearands;
-    // Expression( Types * type, int num_op );
-
     Expression() : Non_Terminal( "" ){};
 };
-
-// Expression::Expression(){}
-
-//-------------------------------------------------
 class PrimaryExpression : public Expression {
-    /**
-isTerminal = 0 when ( Expression )
-isTerminal = 1 when IDENTIFIER
-isTerminal = 2 when CONSTANT
-isTerminal = 3 when STRING_LITERAL
-*/
   public:
     int isTerminal;
-    Identifier *Ival;
-    StringLiteral *Sval;
-    Constant *Cval;
     Expression *op1;
-
+    Identifier *Ival;
+    Constant *Cval;
+    StringLiteral *Sval;
+    
     PrimaryExpression() {
         isTerminal = 0;
-        Ival = nullptr;
-        Sval = nullptr;
-        Cval = nullptr;
         op1 = nullptr;
+        Ival = nullptr;
+        Cval = nullptr;
+        Sval = nullptr;
     }
 };
 
-// Grammar warppers for PrimaryExpression
-Expression *create_primary_identifier( Identifier *id );
 Expression *create_primary_expression( Expression *exp );
-
-//-------------------------------------------------
+Expression *create_primary_identifier( Identifier *id );
 
 class ArgumentExprList : public Expression {
   public:
-    Expression *op1; // This will be null in case of root object
+    Expression *op1; 
     Expression *op2;
 
     ArgumentExprList() {
@@ -72,238 +56,208 @@ class ArgumentExprList : public Expression {
 };
 
 
-//-------------------------------------------------
 class UnaryExpression : public Expression {
   public:
-    Expression *op1;
     string op;
+    Expression *op1;
 
-    UnaryExpression() {
-        op1 = nullptr;
+    UnaryExpression() { 
         op = "";
+        op1 = nullptr;
     }
 };
 
-// Grammar warppers for UnaryExpression
-Expression *
-create_unary_expression_ue( string unary_op,
-                            Expression *unary_exp ); // INC_OP, DEC_OP, SIZEOF
 Expression *create_unary_expression_cast( Node *node_op, Expression *exp );
+Expression *create_unary_expression_ue( string unary_op,Expression *unary_exp ); 
 
-//-------------------------------------------------
 class CastExpression : public Expression {
   public:
-    Expression *op1;
-    /**
-      Index of Type casted to in GlobalTypes
-      -1 if there is no casting
-    */
     int typeCast;
+    Expression *op1;
 
     CastExpression() {
-        op1 = nullptr;
         typeCast = -1;
-    };
-};
-
-//-------------------------------------------------
-class MultiplicativeExpression : public Expression {
-  public:
-    Expression *op1;
-    Expression *op2;
-    string op;
-
-    MultiplicativeExpression() {
         op1 = nullptr;
-        op2 = nullptr;
-        op = "";
     };
 };
-Expression *create_multiplicative_expression( string opr, Expression *exp1,
-                                              Expression *exp2 );
 
-//-------------------------------------------------
 class AdditiveExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     AdditiveExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     };
 };
-Expression *create_additive_expression( string opr, Expression *exp1,
-                                        Expression *exp2 );
+Expression *create_additive_expression( string opr, Expression *exp1, Expression *exp2 );
 
-//-------------------------------------------------
-class ShiftExpression : public Expression {
+class MultiplicativeExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
-    ShiftExpression() {
+    MultiplicativeExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     };
 };
+Expression *create_multiplicative_expression( string opr, Expression *exp1, Expression *exp2 );
 
-Expression *create_shift_expression( string opr, Expression *exp1,
-                                     Expression *exp2 );
-
-//-------------------------------------------------
 class RelationalExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     RelationalExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     };
 };
-Expression *create_relational_expression( string opr, Expression *exp1,
-                                          Expression *exp2 );
+Expression *create_relational_expression( string opr, Expression *exp1, Expression *exp2 );
 
-//-------------------------------------------------
+class ShiftExpression : public Expression {
+  public:
+    string op;
+    Expression *op1;
+    Expression *op2;
+
+    ShiftExpression() {
+        op = "";
+        op1 = nullptr;
+        op2 = nullptr;
+    };
+};
+Expression *create_shift_expression( string opr, Expression *exp1, Expression *exp2 );
+
 class EqualityExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     EqualityExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     };
 };
-Expression *create_equality_expression( string opr, Expression *exp1,
-                                        Expression *exp2 );
+Expression *create_equality_expression( string opr, Expression *exp1, Expression *exp2 );
 
-//--------------------------------------------------
+
 class AndExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     AndExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     };
 };
-Expression *create_and_expression( string opr, Expression *exp1,
-                                   Expression *exp2 );
+Expression *create_and_expression( string opr, Expression *exp1, Expression *exp2 );
 
-//-------------------------------------------------
+
 class ExclusiveorExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     ExclusiveorExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     };
 };
-Expression *create_exclusive_or_expression( string opr, Expression *exp1,
-                                            Expression *exp2 );
+Expression *create_exclusive_or_expression( string opr, Expression *exp1, Expression *exp2 );
 
-//-------------------------------------------------
+
 class InclusiveorExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     InclusiveorExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     }
 };
 Expression *create_inclusive_or_expression( string opr, Expression *exp1, Expression *exp2 );
 
-//-------------------------------------------------
 class Logical_andExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     Logical_andExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     };
 };
-Expression *create_logical_and_expression( string opr, Expression *exp1,
-                                           Expression *exp2 );
+Expression *create_logical_and_expression( string opr, Expression *exp1, Expression *exp2 );
 
-//-------------------------------------------------
+
 class Logical_orExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     Logical_orExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     };
 };
-Expression *create_logical_or_expression( string opr, Expression *exp1,
-                                          Expression *exp2 );
+Expression *create_logical_or_expression( string opr, Expression *exp1, Expression *exp2 );
 
-//-------------------------------------------------
+
 class ConditionalExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
     Expression *op3;
-    string op;
 
     ConditionalExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
         op3 = nullptr;
-        op = "";
     };
 };
-Expression *create_conditional_expression( string opr, Expression *exp1,
-                                           Expression *exp2, Expression *exp3 );
+Expression *create_conditional_expression( string opr, Expression *exp1, Expression *exp2, Expression *exp3 );
 
-//-------------------------------------------------
+
 class AssignmentExpression : public Expression {
   public:
+    string op;
     Expression *op1;
     Expression *op2;
-    string op;
 
     AssignmentExpression() {
+        op = "";
         op1 = nullptr;
         op2 = nullptr;
-        op = "";
     }
 };
 
-Expression *create_assignment_expression(
-    Expression *exp1, Node *node_opr,
-    Expression
-        *exp2 ); // can change string to node* later for assignment operator
+Expression *create_assignment_expression(Expression *exp1, Node *node_opr, Expression *exp2 ); 
 
-//-------------------------------------------------
 class TopLevelExpression : public Expression {
   public:
     Expression *op1;
@@ -315,27 +269,27 @@ class TopLevelExpression : public Expression {
     };
 };
 
-//-------------------------------------------------
+
 class ConstantExpression : public Expression {
   public:
     Expression *op1;
 };
 
-//-------------------------------------------------
+
 class PostfixExpression : public Expression {
   public:
     PostfixExpression *pe;
-    Expression *exp;
-    Identifier *id;
-    ArgumentExprList *ae_list;
     string op;
+    Identifier *id;
+    Expression *exp;
+    ArgumentExprList *ae_list;
 
     PostfixExpression() {
         pe = nullptr;
-        exp = nullptr;
-        id = nullptr;
-        ae_list = nullptr;
         op = "";
+        id = nullptr;
+        exp = nullptr;
+        ae_list = nullptr;
     };
 };
 
@@ -344,57 +298,63 @@ class Constant : public Terminal {
     Constant( const char *name );
 
     Type getConstantType() {
-        Type retT( 2, 0, false );
-        int length = value.length();
-        if ( name == "CONSTANT HEX" || name == "CONSTANT INT" ) {
-          
-            int islong = 0, isunsigned = 0;
-            for ( int i = 0; i < length; i++ ) {
-                if ( value[i] == 'l' || value[i] == 'L' )
-                    islong = 1;
-                if ( value[i] == 'u' || value[i] == 'U' )
-                    isunsigned = 1;
-                if ( islong && isunsigned ){
-                    retT.typeIndex = PrimitiveTypes::U_LONG_T;
-                    return retT;
-                }
-            }
-            if ( islong ) {
-                retT.typeIndex = PrimitiveTypes::LONG_T;
-                return retT;
-            }
-            if ( isunsigned ) {
-                retT.typeIndex = PrimitiveTypes::U_INT_T;
-                return retT;
-            }
-            retT.typeIndex = PrimitiveTypes::INT_T;
-            return retT;
-            // loop over value to get unsigned etc and return typeIndex
-        } else if ( name == "CONSTANT FLOAT" ) {
-            for ( int i = 0; i < length; i++ ) {
-                
-                if ( value[i] == 'f' || value[i] == 'F') {
-                    retT.typeIndex = PrimitiveTypes::FLOAT_T;
-                    return retT;
-                }
+        Type returnT( 2, 0, false );
+        int len = value.length();
+        if ( name == "CONSTANT INT"  || name == "CONSTANT HEX" ) {        
+            int type_long = 0, type_unsigned = 0, i = 0;
+            while(i<len){
+                if ( value[i] == 'u' )
+                    type_unsigned = 1;
+                if ( value[i] == 'U' )
+                    type_unsigned = 1;
+                if ( value[i] == 'l' ) 
+                    type_long = 1;
+                if ( value[i] == 'L' ) 
+                    type_long = 1;                  
+                if( type_long != 0 && type_unsigned != 0){
+                  returnT.typeIndex = PrimitiveTypes::U_LONG_T;
+                  return returnT;
+                }                
+                i++;
             }
             
-            retT.typeIndex = PrimitiveTypes::DOUBLE_T;
-
-            return retT;
-            // loop over value to get float
-        } else if ( name == "CONSANT EXP" ) {
-            // loop over value to get if long or double
-            for ( int i = 0; i < length; i++ ) {
-                if ( value[i] == 'f' || value[i] == 'F' ) {
-                    retT.typeIndex = PrimitiveTypes::FLOAT_T;
-                    return retT;
+            if ( type_unsigned != 0 ) {
+                returnT.typeIndex = PrimitiveTypes::U_INT_T;
+                return returnT;
+            }
+            if ( type_long != 0 ) {
+                returnT.typeIndex = PrimitiveTypes::LONG_T;
+                return returnT;
+            }
+            
+            returnT.typeIndex = PrimitiveTypes::INT_T;
+            return returnT;
+            
+        } else if ( name == "CONSTANT EXP" ) {
+            
+            int i=0;
+            while(i<len){
+                if ( value[i] == 'F' || value[i] == 'f' ) {
+                    returnT.typeIndex = PrimitiveTypes::FLOAT_T;
+                    return returnT;
                 }
             }
-            retT.typeIndex = PrimitiveTypes::LONG_T;
-            return retT;
-        } else {
-            return retT;
+            returnT.typeIndex = PrimitiveTypes::LONG_T;
+            return returnT;
+        } else if ( name == "CONSTANT FLOAT" ) {
+            
+            int i=0;
+            while(i<len){                
+                if ( value[i] == 'F' || value[i] == 'f' ) {
+                    returnT.typeIndex = PrimitiveTypes::FLOAT_T;
+                    return returnT;
+                }
+                i++;
+            }         
+            returnT.typeIndex = PrimitiveTypes::DOUBLE_T;
+            return returnT;
+        }  else {
+            return returnT;
         }
     }
 };
@@ -405,9 +365,9 @@ class StringLiteral : public Terminal {
 };
 
 Expression *create_postfix_expr_arr( Expression *p_exp, Expression *exp );
-Expression *create_postfix_expr_struct( string str, Expression *exp,
-                                        Identifier *id );
 Expression *create_postfix_expr_ido( string opr, Expression *p_exp );
+Expression *create_postfix_expr_struct( string str, Expression *exp,Identifier *id );
 Expression *create_toplevel_expression( Expression *exp1, Expression *exp2 );
+
 #endif
 
